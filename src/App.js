c986a36cb8d5108090747bd83e9b9c8a580caa63
woojin <tbvc@naver.com>
2023-12-07 10:08:37 +0900

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import About from './About';
